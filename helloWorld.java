01d8b2dc3f304cdc60f534b8eb286a251e218521
Edward <edward.sun2015@gmail.com>
2021-03-26 18:09:03 -0700

    added loop to hello world java file

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,9 @@
 public static class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello WRLD!");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
