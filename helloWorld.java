daffa6a71e8d5206d30ece12bfbf60589896f603
Edward <edward.sun2015@gmail.com>
2021-04-04 22:51:40 -0700

    hello world while loop update

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,12 @@
 public static class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello WRLD!");
-
+        int j = 0;
+        while (j < 10) {
+            if (j%2 == 0) {
+                System.out.println("EVEN: " + j);
+            }
+        }
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
         }
