459508dbd0af55a2456a140c2986cdebe9db329f
Edward <edward.sun2015@gmail.com>
2021-04-05 11:57:15 -0700

    added to the print output of test.java

--- test.java
+++ test.java
@@ -2,6 +2,12 @@ public class test.java {
     public static void main(String[] args) {
         int i = 1;
         System.out.println(i);
+
+        while (i<10) {
+            if (i%2 + 1 > 2) {
+                System.out.println("flag");
+            }
+        }
     }
 }
 
