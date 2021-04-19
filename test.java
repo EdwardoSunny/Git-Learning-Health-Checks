186978bd8a816804a47dbd11387348c756647eb6
edward <edward.sun2015@gmail.com>
2021-04-19 09:56:59 -0700

    test.java fix and change

--- test.java
+++ test.java
@@ -1,7 +1,10 @@
 public class test.java {
     public static void main(String[] args) {
-        int i = 1;
-        System.out.println(i);
+        int[] testingList = {1, 2, 3, 4, 5, 6, 7};
+
+	for (int i = 0; i < 10; i++) {
+		System.out.println(i);
+	}
     }
 }
 
