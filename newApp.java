99c46fb499bde2cdea91be9b57848abda553fe6a
edward <edward.sun2015@gmail.com>
2021-04-19 10:07:59 -0700

    newApp.java add

---
+++ newApp.java
@@ -0,0 +1,10 @@
+public class newApp {
+	public static void main(String[] args) {
+		System.out.println("hello this is cool new app");
+		while (true) {
+			System.out.println("hello");
+		}
+	
+	}
+
+}
