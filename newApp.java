ef13ba326654948481b533597743a374383169e2
edward <edward.sun2015@gmail.com>
2021-04-21 09:19:05 -0700

    newApp  test stuff

--- newApp.java
+++ newApp.java
@@ -2,9 +2,12 @@ public class newApp {
 	public static void main(String[] args) {
 		System.out.println("hello this is cool new app");
 		while (true) {
-			System.out.println("hello");
-		}
-	
+			int i = 10;
+			if (i % 2) {
+				System.out.println("even");
+			}
+			i++;
+		}	
 	}
 
 }
